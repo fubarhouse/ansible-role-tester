649b4ce9b0cc2c8eeca9fb853cef72ea6e3a411a
Karl Hepworth <karl.hepworth@gmail.com>
2018-06-11 20:59:17 +1000

    Updates the test command to be user friendly.

--- cmd/test.go
+++ cmd/test.go
@@ -9,13 +9,11 @@ import (
 // testCmd represents the test command
 var testCmd = &cobra.Command{
 	Use:   "test",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Execute tests against an existing container",
+	Long: `Execute tests against an existing container
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+If container does not exist it will be created, however
+containers won't be removed after completion.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		config := AnsibleConfig{
 			source,
@@ -28,19 +26,18 @@ to quickly create a Cobra application.`,
 		if e != nil {
 			logrus.Errorln("Incompatible disribution was inputted, attempting autofix.")
 		}
-		dist.run(&config)
+
 		dist.install(&config)
 		dist.test_syntax(&config)
 		dist.test_role(&config)
 		dist.test_idempotence(&config)
-		kill()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(testCmd)
 	pwd, _ := os.Getwd()
-	testCmd.Flags().StringVarP(&containerID, "name", "n", containerID, "Name of the container")
+	testCmd.Flags().StringVarP(&containerID, "id", "n", containerID, "Container ID")
 	testCmd.Flags().StringVarP(&source, "source", "s", pwd, "Location of the role to test")
 	testCmd.Flags().StringVarP(&destination, "destination", "d", "/etc/ansible/roles/role_under_test", "Location which the role will be mounted to")
 	testCmd.Flags().StringVarP(&requirements, "requirements", "r", "", "Path to requirements file.")
