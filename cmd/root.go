e3a0cb1edb8ea5947e9169f42ededb73325b3b19
Karl Hepworth <karl.hepworth@gmail.com>
2018-06-26 22:14:10 +1000

    Removes long description from root
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -68,12 +68,7 @@ var (
 	rootCmd = &cobra.Command{
 		Use:   "ansible-test",
 		Short: "Run an Ansible role for testing purposes in an isolated environment.",
-		Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+		Long: ``,
 	}
 )
 
