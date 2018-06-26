3e7b98fa70da4cadb2fdbe769ca607eb6045f673
Karl Hepworth <karl.hepworth@gmail.com>
2018-06-26 22:12:58 +1000

    Get the shell command working!
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- cmd/shell.go
+++ cmd/shell.go
@@ -15,27 +15,28 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
-)
+	"github.com/fubarhouse/ansible-role-tester/util"
+	)
 
 // shellCmd represents the shell command
 var shellCmd = &cobra.Command{
 	Use:   "shell",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Shells into a container",
+	Long: `Shell into a container after creation.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("shell called")
+		arguments := []string{
+			"exec",
+			"-it",
+			containerID,
+			"bash",
+		}
+		util.DockerExec(arguments, true)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(shellCmd)
-	shellCmd.Flags().StringVarP(&containerID, "name", "n", "", "Name of the container")
+	shellCmd.Flags().StringVarP(&containerID, "name", "n", containerID, "Container ID")
+	shellCmd.MarkFlagRequired("name")
 }
